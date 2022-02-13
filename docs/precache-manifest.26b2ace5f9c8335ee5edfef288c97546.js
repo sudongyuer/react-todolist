ad08bacc19b6b2fc3dd9201a0b7727a4fb68ee90
sudongyu <976499226@qq.com>
2022-02-13 17:20:40 +0800

    feat: add docs

---
+++ docs/precache-manifest.26b2ace5f9c8335ee5edfef288c97546.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f8cdebdca8e877ab85a4eb81f96aa24",
+    "url": "/sudongyuer/react-todolist/index.html"
+  },
+  {
+    "revision": "6736951040b78aa8d2fc",
+    "url": "/sudongyuer/react-todolist/static/css/main.3ec646e0.chunk.css"
+  },
+  {
+    "revision": "b31df5b6aeb30e3b8574",
+    "url": "/sudongyuer/react-todolist/static/js/2.de0d90ba.chunk.js"
+  },
+  {
+    "revision": "6736951040b78aa8d2fc",
+    "url": "/sudongyuer/react-todolist/static/js/main.dc338c72.chunk.js"
+  },
+  {
+    "revision": "43dd1227ba16d377f01b",
+    "url": "/sudongyuer/react-todolist/static/js/runtime~main.92833da0.js"
+  }
+]);
